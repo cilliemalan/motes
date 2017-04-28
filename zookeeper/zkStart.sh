77355ae34467c60bad1c4b5069d43b0a451eb2f9
Cillié Malan <cilliemalan@gmail.com>
2017-04-28 13:39:54 +0200

    Slightly more streamlined zookeeper

--- zookeeper/zkStart.sh
+++ zookeeper/zkStart.sh
@@ -2,5 +2,11 @@
 
 # Helper script to start the service as the right user and config
 
+# change to zookeeper dir
+cd /opt/zookeeper
+
+# make config
+cat /conf/zoo.cfg.base > zoo.cfg
+
 
 su zookeeper -c 'zkServer.sh start-foreground'

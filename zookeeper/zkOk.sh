ee778f97d97e30ea64d03296a2e0618658d5fd83
Cillié Malan <cilliemalan@gmail.com>
2017-04-28 11:25:56 +0200

    Zookeeper dockerfile

---
+++ zookeeper/zkOk.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+if [ "`echo ruok | nc 127.0.0.1 2181`" == "imok" ]; then
+	exit 0
+else
+	exit 1
+fi
---
+++ zookeeper/zkStats.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+echo mntr | nc 127.0.0.1 2181 >& 1

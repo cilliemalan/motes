6c8ce60261487b9bac7cbcea012ea0bc61523f5e
Cillié Malan <cilliemalan@gmail.com>
2017-05-02 19:22:03 +0200

    some logging for web

--- web/index.js
+++ web/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+console.log('starting...');
+
+// change cwd to where this script is
+process.chdir(__dirname);
+
+console.log(`running in ${process.cwd()}`);
 
 const port = process.env.PORT || 3000;
-const wwwroot = "public";
+const wwwroot = path.resolve("./public");
 
+console.log(`static files in ${wwwroot}`);
 app.use(express.static(wwwroot));
 
+console.log(`start listen on port ${port}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 

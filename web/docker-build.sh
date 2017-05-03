fa72dd48829ae5a6d8a71e14e0aaef658888e7c3
Cillié Malan <cilliemalan@gmail.com>
2017-05-03 21:40:42 +0200

    docker build script for web

--- web/docker-build.sh
+++ web/docker-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker build -t cilliemalan/motes-web .
+HOST=eu.gcr.io
+PROJECT=modern-kiln-165813
+REPO=motes-web
+VER=latest
+TAG="$HOST/$PROJECT/$REPO:$VER"
+
+docker build -t "$TAG" --cache-from "$TAG" .

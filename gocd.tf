5201de36d626ca88d2655745d04d0d6885ebbcac
Cillié Malan <cilliemalan@gmail.com>
2017-05-06 21:27:17 +0200

    turns out gocd server needs compute-rw to be able to create compute resources. Who knew.

--- gocd.tf
+++ gocd.tf
@@ -66,7 +66,7 @@ resource "google_compute_instance" "gocd" {
   }
 
   service_account {
-    scopes = ["useraccounts-ro", "storage-ro", "logging-write",
+    scopes = ["useraccounts-ro", "storage-ro", "logging-write", "compute-rw",
               "monitoring-write", "service-management", "service-control",
               "https://www.googleapis.com/auth/ndev.clouddns.readwrite"]
   }

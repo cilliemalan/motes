f25e16918643d24764d7428e30db0d674fcefdcb
Cillié Malan <cilliemalan@gmail.com>
2017-05-08 17:42:05 +0200

    Fleshed out pretest script

--- build-scripts/run-pretests.sh
+++ build-scripts/run-pretests.sh
@@ -2,3 +2,14 @@
 
 # this script runs tests outside of the run environment. These are typically unit tests
 # that don't require any integration
+
+
+# run inside proj dir and use project env
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "$DIR"
+source "build-scripts/utilities/project-env.sh"
+
+cd web
+npm install
+npm run test
+

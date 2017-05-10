c66e0b9e3da137c172cee1898f9c15a71860b9a7
Cillié Malan <cilliemalan@gmail.com>
2017-05-10 21:26:45 +0200

    prepare debugging is now just called run and will also open a browser window

--- web/run.sh
+++ web/run.sh
@@ -8,11 +8,44 @@ cd "$DIR"
 # setup project env
 source ../build-scripts/utilities/project-env.sh
 
+echo "configuring debug services..."
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-debug-debugger
+  labels:
+    app: web-debug-debugger
+spec:
+  ports:
+    - name: debugger
+      port: 5858
+      nodePort: 31858
+  type: NodePort
+  selector:
+    app: debug-runner
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: web-debug
+  labels:
+    app: web-debug
+spec:
+  ports:
+    - name: http
+      port: 3000
+      nodePort: 31000
+  type: NodePort
+  selector:
+    app: debug-runner
+EOF
+
 
 if [[ -z "$(kubectl get pods -l "app==debug-runner" | grep Running)" ]]; then
-  
 
-  kubectl create -f - <<EOF
+
+kubectl create -f - <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
@@ -21,18 +54,23 @@ metadata:
     app: debug-runner
 spec:
   containers:
-  - name: debug-runner
-    image: $(imagetag web)
-    imagePullPolicy: IfNotPresent
-    command: ["sleep"]
-    args: ["100000"]
-    env:
-    - name: NODE_ENV
-      value: development
-    - name: PORT
-      value: "3000"
-    - name: DEBUG_PORT
-      value: "5858"
+    - name: debug-runner
+      image: $(imagetag web)
+      imagePullPolicy: IfNotPresent
+      command: ["sleep"]
+      args: ["100000"]
+      ports:
+        - name: debug
+          containerPort: 5858
+        - name: http
+          containerPort: 3000
+      env:
+        - name: NODE_ENV
+          value: development
+        - name: PORT
+          value: "3000"
+        - name: DEBUG_PORT
+          value: "5858"
 EOF
 
   # wait for it to be ready
@@ -62,6 +100,9 @@ kubectl exec debug-runner -- bash -c 'PIDS=$(ps -A | grep node | egrep -o "^ *[0
 
 echo "Running npm inside pod"
 kubectl exec -i debug-runner -- npm install
+
+echo "Opening in browser..."
+minikube service web-debug
 
 echo "running program inside pod"
 kubectl exec debug-runner -- node --debug=0.0.0.0:5858 -nolazy index.js

78dbbd45c26aa460f1416cc28f49b4874d7c9cc7
Cillié Malan <cilliemalan@gmail.com>
2017-05-10 21:27:29 +0200

    routing api

--- web/index.js
+++ web/index.js
@@ -1,19 +1,34 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const api = require('./api');
 
 console.log('starting...');
 
 // change cwd to where this script is
 process.chdir(__dirname);
 
-console.log(`running in ${process.cwd()}`);
-
+// some vars
 const port = process.env.PORT || 3000;
 const wwwroot = path.resolve("./public");
 
+console.log(`running in ${process.cwd()}`);
+
+// the express app
+const app = express();
+
+// static files
 console.log(`static files in ${wwwroot}`);
 app.use(express.static(wwwroot));
 
+// some parsers
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// our api
+app.use('/api', api);
+
+// start!
 console.log(`start listen on port ${port}`);
 app.listen(port, () => console.log(`Listening on port ${port}`));
+

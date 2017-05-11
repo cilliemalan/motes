fb5bb7a37c3d34e54d4c70b170494adc71457a96
Cillié Malan <cilliemalan@gmail.com>
2017-05-11 17:37:58 +0200

    now making bigger clusters

--- build-scripts/prepare-environment.sh
+++ build-scripts/prepare-environment.sh
@@ -24,14 +24,14 @@ NODES=0
 NODE_TYPE="n1-standard-1"
 CLUSTER_NAME="$LABEL-cluster"
 if [[ "$LABEL" == "dev" ]]; then
-    NODES=3
-    NODE_TYPE="n1-standard-1"
-elif [[ "$LABEL" == "test" ]]; then
-    NODES=3
-    NODE_TYPE="n1-standard-1"
-elif [[ "$LABEL" == "prod" ]]; then
     NODES=5
-    NODE_TYPE="n1-standard-1"
+    NODE_TYPE="n1-standard-2"
+elif [[ "$LABEL" == "test" ]]; then
+    NODES=5
+    NODE_TYPE="n1-standard-2"
+elif [[ "$LABEL" == "prod" ]]; then
+    NODES=10
+    NODE_TYPE="n1-standard-4"
 else
     echo "Unknown environment name $LABEL. must be dev, test, or prod."
     exit 2;
@@ -44,9 +44,9 @@ then
 
     gcloud container --project "$PROJECT_ID" clusters create "$CLUSTER_NAME" $ZONEPARM \
         --cluster-version=1.6.2 \
-        --machine-type "n1-standard-1" --image-type "COS" --disk-size "100" \
+        --machine-type "$NODE_TYPE" --image-type "COS" --disk-size "100" \
         --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-        --num-nodes "3" --network "default" --enable-cloud-logging --no-enable-cloud-monitoring
+        --num-nodes "$NODES" --network "default" --enable-cloud-logging --no-enable-cloud-monitoring
 
 else
     echo "$LABEL-cluster exists"

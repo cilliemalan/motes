21ed6c677a3773360d4bd58f33aa5bd467e56cd9
Cillié Malan <cilliemalan@gmail.com>
2017-05-11 09:51:53 +0200

    Fixed reconf

--- web-proxy/reconf.sh
+++ web-proxy/reconf.sh
@@ -6,27 +6,32 @@ cd /usr/local/etc/haproxy
 WEB_HOSTS=$(host web | egrep -oe '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
 CANARY_HOSTS=$(host web-canary | egrep -oe '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
 
-
 printserver() {
-    local server=$1
+    local name=$1
+    local server=$2
     shift
-    echo "    server public $server:8080 $@"
+    shift
+    echo "    server $name $server:8080 $@"
 }
 
 printservers() {
-    local servers=$1
+    local name=$1
+    local servers=$2
+    local increment=0
+    shift
     shift
     for s in $servers; do
-        printserver $s "$@"
+        increment=$((increment+1))
+        printserver "$name$increment" $s "$@"
     done
 }
 
 printweb() {
-    printservers "$WEB_HOSTS" weight 95 "$@"
+    printservers "web" "$WEB_HOSTS" weight 19 "$@"
 }
 
 printcanary() {
-    printservers "$CANARY_HOSTS" weight 5 "$@"
+    printservers "canary" "$CANARY_HOSTS" weight 1 "$@"
 }
 
 

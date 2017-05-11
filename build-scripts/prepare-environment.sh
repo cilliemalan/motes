d1aa58f0ab5264330f1131c00c5ae0624bcefec9
Cillié Malan <cilliemalan@gmail.com>
2017-05-11 17:16:04 +0200

    k8s version

--- build-scripts/prepare-environment.sh
+++ build-scripts/prepare-environment.sh
@@ -43,6 +43,7 @@ then
     echo "Creating $LABEL-cluster in ${ZONE:-the default zone}"
 
     gcloud container --project "$PROJECT_ID" clusters create "$CLUSTER_NAME" $ZONEPARM \
+        --cluster-version=1.6.2 \
         --machine-type "n1-standard-1" --image-type "COS" --disk-size "100" \
         --scopes "https://www.googleapis.com/auth/compute","https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
         --num-nodes "3" --network "default" --enable-cloud-logging --no-enable-cloud-monitoring

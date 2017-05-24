350983476bef266278d0ec7b66968f411fd9d938
Cillié Malan <cilliemalan@gmail.com>
2017-05-24 14:40:50 +0200

    some more rename updates

--- build-scripts/run-e2e-tests.sh
+++ build-scripts/run-e2e-tests.sh
@@ -14,7 +14,7 @@ red() { >&2 echo -e "\033[0;31m$@\033[0m"; }
 green() { echo -e "\033[0;32m$@\033[0m"; }
 
 #prepare unit test pod
-build-scripts/utilities/prepare-unit-test-pod.sh
+build-scripts/utilities/prepare-dev-pod.sh
 
 
 # run tests

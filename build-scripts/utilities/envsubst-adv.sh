d8446201b40dce38e92b454bb10bf6512c8855b8
Cillié Malan <cilliemalan@gmail.com>
2017-06-11 19:27:22 +0200

    fixed big problems in envsubst

--- build-scripts/utilities/envsubst-adv.sh
+++ build-scripts/utilities/envsubst-adv.sh
@@ -6,17 +6,33 @@
 # example:
 # $ echo 'We are in $(pwd) and profile is at $HOME' | ./envsubst-adv.sh
 
-transform_piped() {
-    echo start
-    while read x ; do echo "-$x" ; done
-    echo end
-}
+
+# run inside proj dir and use project env
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+cd "$DIR"
+source "build-scripts/utilities/project-env.sh"
+
+
+
+# the EOF for the heredoc
 EOF=$(cat /dev/urandom | tr -dc 'A-Z' | fold -w 32 | head -n 1)
+
+# start of the heredoc
 INPUT="cat <<$EOF"
-while read x ; do
-    INPUT=$(printf "%s\n%s" "$INPUT" "$x")
+
+# read input into the heredoc
+while IFS= read -r line; do
+  INPUT=$(printf "%s\n%s" "$INPUT" "$line")
 done
+
+# close off the heredoc
 INPUT=$(printf "%s\n$EOF\n\n" "$INPUT")
 
+# defaults for vars
+CONTAINER_VERSION=${CONTAINER_VERSION:-latest}
+SCALE=${SCALE:-1}
+DATADISKSIZE=${DATADISKSIZE:-1Gi}
+
+# evaluate the input
 eval "$INPUT"
 

3a6137af9ceaa97aa14e13b9f0bda69a5ab81d2b
Cillié Malan <cilliemalan@gmail.com>
2017-06-11 21:37:56 +0200

    downloading a few prereqs

--- build-scripts/prerequisites.sh
+++ build-scripts/prerequisites.sh
@@ -16,12 +16,37 @@ cd web
 green "Using npm version $(npm --version)"
 echo "Running npm install"
 
+# track status
+FAILED=0
+
 if npm install; then
     green npm install succeeded
-    exit 0
 else
     red npm install failed
-    exit 1
+    FAILED=1
 fi
 
+if [[ ! -e "$DIR/.cache" ]]; then
+    echo "Creating cache dir"
+    mkdir -p "$DIR/.cache"
+fi
 
+if [[ ! -f "$DIR/.cache/kubectl" ]]; then
+    echo "Downloading linux kubectl"
+    curl -L -o "$DIR/.cache/kubectl" https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +x "$DIR/.cache/kubectl"
+fi
+
+if [[ ! -f "$DIR/.cache/cfssl" ]]; then
+    echo "Downloading cfssl"
+    curl -L -o "$DIR/.cache/cfssl" https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+    chmod +x "$DIR/.cache/cfssl"
+fi
+
+if [[ ! -f "$DIR/.cache/cfssljson" ]]; then
+    echo "Downloading cfssljson"
+    curl -L -o "$DIR/.cache/cfssljson" https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+    chmod +x "$DIR/.cache/cfssljson"
+fi
+
+    

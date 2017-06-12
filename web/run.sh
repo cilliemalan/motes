bad729edaa935b5ba824fef6132a259f2fe8ebe8
Cillié Malan <cilliemalan@gmail.com>
2017-06-12 19:40:12 +0200

    run script now uses inspect

--- web/run.sh
+++ web/run.sh
@@ -10,11 +10,6 @@ source ../build-scripts/utilities/project-env.sh
 
 echo "Killing any running node instances inside container"
 echo "running program inside pod"
-kubectl exec local-dev -- bash -c 'PIDS=$(ps -A | grep node | egrep -o "^ *[0-9]+" | egrep -o "[0-9]+");for pid in $PIDS; do echo "killing $pid"; kill $pid; done; node --debug=0.0.0.0:5858 -nolazy index.js' &
+kubectl exec local-dev -- bash -c 'PIDS=$(ps -A | grep node | egrep -o "^ *[0-9]+" | egrep -o "[0-9]+");for pid in $PIDS; do echo "killing $pid"; kill $pid; done; node --inspect=0.0.0.0:5858 -nolazy index.js'
 
 
-# echo "Opening in browser..."
-# minikube service local-dev
-
-disown
-exit 0

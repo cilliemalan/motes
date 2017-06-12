a9e02a5de4f702d5419ab4e9e51a64668e161ada
Cillié Malan <cilliemalan@gmail.com>
2017-06-12 13:45:11 +0200

    Pretest now actually lints

--- build-scripts/run-pretests.sh
+++ build-scripts/run-pretests.sh
@@ -9,7 +9,28 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$DIR"
 source "build-scripts/utilities/project-env.sh"
 
+red() { >&2 echo -e "\033[0;31m$@\033[0m"; }
+green() { echo -e "\033[0;32m$@\033[0m"; }
+
+
+RESULT=0
+
 cd web
 npm install
+if [[ $? != 0 ]]; then red "failed to NPM install"; RESULT=$?; fi
+
 npm run test
+if [[ $? != 0 ]]; then red "failed to run tests"; RESULT=$?; fi
+
+node_modules/.bin/eslint .
+if [[ $? != 0 ]]; then red "There were linting problems"; RESULT=$?; fi
+
+
+if [[ $RESULT == 0 ]]; then
+    green No problems found
+    exit 0
+else
+    red There were problems!
+    exit 1
+fi
 

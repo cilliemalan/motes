0a32b910eada988428c57c4e100419f3f9ed79a9
Cillié Malan <cilliemalan@gmail.com>
2017-06-12 22:16:11 +0200

    loading secrets on startup

--- web/app/index.js
+++ web/app/index.js
@@ -4,39 +4,61 @@ const bodyParser = require('body-parser');
 const api = require('./api');
 const zk = require('./zookeeperProvider');
 
-console.log('starting...');
-
-// change cwd to the application root
-process.chdir(path.join(__dirname, '..'));
-
 // some vars
 const port = process.env.PORT || 3000;
 const wwwroot = path.resolve("./public");
 
-console.log(`running in ${process.cwd()}`);
+/**
+ * The main entrypoint function.
+ */
+async function main() {
 
-// the express app
-const app = express();
+    console.log('starting...');
 
-// static files
-console.log(`static files in ${wwwroot}`);
-app.use(express.static(wwwroot));
+    // change cwd to the application root
+    process.chdir(path.join(__dirname, '..'));
 
-// some parsers
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+    console.log(`running in ${process.cwd()}`);
 
-// our api
-app.use('/api', api);
+    // initialize secrets
+    await require('./secrets').initializeAsync();
 
-// start!
-console.log(`start listen on port ${port}`);
-app.listen(port, async () => {
+    // the express app
+    const app = express();
+
+    // static files
+    console.log(`static files in ${wwwroot}`);
+    app.use(express.static(wwwroot));
+
+    // some parsers
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    // our api
+    app.use('/api', api);
+
+    // start!
+    console.log(`start listen on port ${port}`);
+
+    // listen (promisified)
+    await new Promise((resolve, reject) => {
+        app.addListener('error', reject);
+
+        app.listen(port, () => {
+            app.removeListener('error', reject);
+            resolve();
+        });
+    });
+
+
     console.log(`Listening on port ${port}`);
-    let path = await zk.registerAsync(process.env.HOSTNAME);
-    console.log(`registered with zk under ${path} as ${process.env.HOSTNAME}`);
+    let zkpath = await zk.registerAsync(process.env.HOSTNAME);
+    console.log(`registered with zk under ${zkpath} as ${process.env.HOSTNAME}`);
     console.log(`There are currently ${await zk.getNumberOfActiveServersAsync()} registered servers`);
-});
+}
+
+//start!
+main();
 
 //provide some data for whomever might need
 module.exports = {

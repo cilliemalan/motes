f5e9735c6ed1fd474ff73cc96c0b63ecb70a8856
Cillié Malan <cilliemalan@gmail.com>
2017-06-13 21:38:11 +0200

    Added mongo tests

--- web/test/integration/mongo.js
+++ web/test/integration/mongo.js
@@ -1,16 +1,57 @@
 const assert = require('chai').assert;
 
-const MongoClient = require('mongodb').MongoClient;
-
 
 describe("Integration", function () {
 
     describe('Mongo', function () {
+
+        let db;
+
+        beforeEach(async function () {
+            db = await require('../../app/integration').mongoConnectAsync();
+        });
+
+        afterEach(async function () {
+            await db.close();
+        });
+
         it('should be accessible', async function () {
 
-            let db = await MongoClient.connect('mongodb://mongo:27017/test');
             assert.isOk(db);
+        });
 
+        it('should be able to create and delete documents', async function () {
+
+            // the item to insert
+            const dbitem = {
+                id: new Date().getTime(),
+                name: 'John',
+                surname: 'Doe',
+                updated: false
+            };
+
+            // get the collection
+            const col = db.collection('testitems');
+
+            // insert it
+            const inserted = await col.insertOne(dbitem);
+
+            // find it again
+            const found = await col.findOne({ id: dbitem.id });
+
+            // and delete it
+            const deleted = await col.deleteOne({ id: found.id });
+
+            assert.isOk(col);
+            assert.isOk(inserted);
+            assert.equal(inserted.insertedCount, 1);
+            assert.isOk(found);
+            assert.equal(found.id, dbitem.id);
+            assert.equal(found.name, dbitem.name);
+            assert.equal(found.surname, dbitem.surname);
+            assert.equal(found.updated, dbitem.updated);
+            assert.isOk(deleted);
+            assert.equal(deleted.deletedCount, 1);
         });
     });
 });

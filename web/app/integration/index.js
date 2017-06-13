1669cb91b5d37e565cd3ada00d238ed032120bea
Cillié Malan <cilliemalan@gmail.com>
2017-06-13 19:18:48 +0200

    Added mongoConnectAsync in integration that actually connects to mongo

--- web/app/integration/index.js
+++ web/app/integration/index.js
@@ -5,6 +5,7 @@ const secrets = require('./secrets');
 const redis = require("redis");
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
+const MongoClient = require('mongodb').MongoClient;
 
 /**
  * Creates a redis client connected to the local k8s redis
@@ -13,6 +14,20 @@ function createRedisClient() {
     return redis.createClient({ host: 'redis', password: secrets.get('redis', 'password') });
 }
 
+/**
+ * Connects to mongo and returns a client
+ */
+async function mongoConnectAsync(database = 'app') {
+    const username = secrets.get("mongo", "user");
+    const password = secrets.get("mongo", "password");
+    const url = `mongodb://${encodeURIComponent(username)}:${encodeURIComponent(password)}@mongo:27017/${database}`;
+
+    const db = await MongoClient.connect(url);
+
+    return db;
+}
+
 module.exports = {
-    createRedisClient
+    createRedisClient,
+    mongoConnectAsync
 };

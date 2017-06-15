cf01afcaded58108f90cffe636a5ec38b07fa513
Cillié Malan <cilliemalan@gmail.com>
2017-06-15 10:32:24 +0200

    Changed app into an application

--- web/app/index.js
+++ web/app/index.js
@@ -5,64 +5,118 @@ const api = require('./api');
 const zk = require('./integration/zookeeper');
 const secrets = require('./integration/secrets');
 const logger = require('winston');
+const http = require('http');
 
-// some vars
-const port = process.env.PORT || 3000;
-const wwwroot = path.resolve("./public");
 
-/**
- * The main entrypoint function.
- */
-async function main() {
+class Application {
 
-    logger.info('starting...');
+    constructor(options = {}) {
+        this._port = options.port || process.env.PORT || 3000;
+        this._wwwroot = options.wwwroot || path.resolve("./public");
+        this._hostname = options.hostname || process.env.HOSTNAME;
+        this._app = express();
+    }
 
-    // change cwd to the application root
-    process.chdir(path.join(__dirname, '..'));
+    async start() {
+        try {
+            if (this._started) {
+                logger.warn('tried to start when already started');
+                return;
+            }
 
-    logger.verbose(`running in ${process.cwd()}`);
+            const app = this._app;
 
-    // initialize secrets
-    await secrets.initializeAsync();
+            logger.info('starting...');
+            logger.verbose(`Going to listen on ${this._port}`);
+            logger.verbose(`Going to server files from ${this._wwwroot}`);
 
-    // the express app
-    const app = express();
+            // initialize secrets
+            await secrets.initializeAsync();
 
-    // static files
-    logger.verbose(`static files in ${wwwroot}`);
-    app.use(express.static(wwwroot));
+            // static files
+            app.use(express.static(this._wwwroot));
 
-    // some parsers
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+            // parsers
+            app.use(bodyParser.urlencoded({ extended: true }));
+            app.use(bodyParser.json());
 
-    // our api
-    app.use('/api', api);
+            // the api
+            app.use('/api', api);
 
-    // start!
-    logger.verbose(`start listen on port ${port}`);
+            // start listening
+            await this._listen();
 
-    // listen (promisified)
-    await new Promise((resolve, reject) => {
-        app.addListener('error', reject);
+            logger.info(`Listening`);
+            logger.info(`Available on ${this.localUrl}`);
+            logger.info(`Available on ${this.publicUrl}`);
 
-        app.listen(port, () => {
-            app.removeListener('error', reject);
-            resolve();
+            // register with zookeeper
+            await this._register();
+
+            this._started = true;
+
+        } catch (e) {
+            logger.error(e);
+            throw e;
+        }
+    }
+
+    async stop() {
+        if (!this._started) {
+            logger.warn('tried to stop while not running. Doing nothing.');
+        } else {
+            logger.info('stopping...');
+
+            // stop listening
+            this._http.close();
+            delete this._http;
+            this._started = false;
+
+            // unregister
+            await zk.unRegisterAsync(this._zkpath);
+            logger.verbose('unregistered from zk');
+            logger.info('stopped');
+
+        }
+    }
+
+    _listen() {
+        return new Promise((resolve, reject) => {
+            const app = this._app;
+            const port = this._port;
+
+            // create the server object
+            this._http = http.createServer(app);
+            this._http.addListener('error', reject);
+
+            // listen and resolve once listening
+            this._http.listen(port, () => {
+                this._http.removeListener('error', reject);
+                resolve();
+            });
         });
-    });
+    }
 
+    async _register() {
+        this._zkpath = await zk.registerAsync(process.env.HOSTNAME);
+        logger.verbose(`registered with zk under ${this._zkpath} as ${process.env.HOSTNAME}`);
+        logger.verbose(`There are currently ${await zk.getNumberOfActiveServersAsync()} registered servers`);
+    }
 
-    logger.info(`Listening on port ${port}`);
-    let zkpath = await zk.registerAsync(process.env.HOSTNAME);
-    logger.verbose(`registered with zk under ${zkpath} as ${process.env.HOSTNAME}`);
-    logger.verbose(`There are currently ${await zk.getNumberOfActiveServersAsync()} registered servers`);
+    get localUrl() {
+        return `http://127.0.0.1:${this._port}`;
+    }
+
+    get publicUrl() {
+        //get our ip address
+        if (!this._ipaddress) {
+            const os = require('os');
+            const ifaces = os.networkInterfaces();
+            this._ipaddress = ifaces['eth0'][0].address;
+        }
+
+        return `http://${this._ipaddress}:${this._port}`;
+    }
 }
 
-//start!
-main();
-
-//provide some data for whomever might need
-module.exports = {
-    url: `http://localhost:${port}/`
-};
+module.exports.Application = Application;
--- web/index.js
+++ web/index.js
@@ -1,1 +1,18 @@
-require('./app');
+const logger = require('winston');
+const Application = require('./app').Application;
+
+//start the application
+const application = new Application();
+application.start();
+
+// graceful shutdown
+async function shutdown() {
+    logger.info('received shutdown signal');
+    await application.stop();
+
+    logger.info('server stopped.');
+    process.exit();
+}
+
+process.once('SIGINT', shutdown);
+process.once('SIGUSR2', shutdown);
